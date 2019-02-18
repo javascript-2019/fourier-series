2c6e48892a6f77af2069c098dedf5afe7a7f503f
marcioDomingues <domingues.marcio@gmail.com>
2019-02-18 23:04:11 +0000

    initial project version

---
+++ sketch.js
@@ -0,0 +1,6 @@
+// Fourier Series
+// Daniel Shiffman
+// https://thecodingtrain.com/CodingChallenges/125-fourier-series.html
+// https://youtu.be/Mm2eYfj0SgA
+// https://editor.p5js.org/codingtrain/sketches/SJ02W1OgV
+
