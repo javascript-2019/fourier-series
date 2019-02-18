35b94939dc53781baabefe559d44e44ef642d6ad
marcioDomingues <domingues.marcio@gmail.com>
2019-02-18 23:50:51 +0000

    fourier series fro square wave

--- sketch.js
+++ sketch.js
@@ -7,46 +7,53 @@
 let time = 0;
 let wave = [];
 
-function setup(){
-    createCanvas(600,400);
+let slider;
+
+function setup() {
+  createCanvas(600, 400);
+  slider = createSlider(1, 10, 5);
 }
 
-function draw(){
-    background(0);
-    translate(200,200);
-    let radius = 100;
+function draw() {
+  background(0);
+  translate(150, 200);
 
+  let x = 0;
+  let y = 0;
+
+  for (let i = 0; i < slider.value(); i++) {
+    let prevx = x;
+    let prevy = y;
+
+    let n = i * 2 + 1;
+    let radius = 75 * (4 / (n * PI));
+    x += radius * cos(n * time);
+    y += radius * sin(n * time);
+
+    stroke(255, 100);
+    noFill();
+    ellipse(prevx, prevy, radius * 2);
+
+    //fill(255);
     stroke(255);
-    noFill();
-    ellipse( 0, 0, radius*2); 
-
-    let x = radius * cos(time);
-    let y = radius * sin(time);
-    //add new point to beginning of the array
-    wave.unshift(y);
-    fill(255);
-    line(0, 0, x, y);
-    ellipse(x, y, 8);
-    
-    translate(200,0);
-    line(x-200, y, 0, wave[0]);
-    beginShape();
-    noFill();
-    for (let i = 0; i < wave.length; i++) {
-        vertex(i, wave[i]);
-    }
-    endShape();
-    
+    line(prevx, prevy, x, y);
+    //ellipse(x, y, 8);
+  }
+  wave.unshift(y);
 
 
+  translate(200, 0);
+  line(x - 200, y, 0, wave[0]);
+  beginShape();
+  noFill();
+  for (let i = 0; i < wave.length; i++) {
+    vertex(i, wave[i]);
+  }
+  endShape();
 
-    time += 0.05
-;
+  time += 0.05;
 
-    //limit the array size
-    if (wave.length > 250){
-        wave.pop();
-    }
+  if (wave.length > 250) {
+    wave.pop();
+  }
 }
-
-
